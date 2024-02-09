96d5ea135f365e78b3ec7e22f3c523438e800f5c
MiguelCock <macockc@eafit.edu.co>
2024-02-09 14:32:58 -0500

    making image data base and uploaing 3 images

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse('a huevo')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Paola Vallejo'})
+
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
+def about(request):
+	return render(request, 'movie/about.html')
