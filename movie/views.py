47ebb583e0a9237f89f263939f5c0db5d462de2e
MiguelCock <macockc@eafit.edu.co>
2024-03-01 00:49:01 -0500

    Solved the issues with the graphs and other errors

--- movie/views.py
+++ movie/views.py
@@ -30,23 +30,23 @@ def stadistics_view(request):
     years = Movie.objects.values_list('year', flat=True).distinct().order_by('year')
     movie_counts_by_year = {}
     for year in years:
-        if year:
-            movies_in_year = Movie.objects.filter(year=year)
+        if year in movie_counts_by_year:
+            movie_counts_by_year[year] += 1
         else:
-             movies_in_year = Movie.objects.filter(year__isnull=True)
-        count = movies_in_year.count()
-        movie_counts_by_year[year] = count
+            movie_counts_by_year[year] = 1
 
     bar_width = 0.5
-    bar_spacing = 0.5
     bar_positions = range(len(movie_counts_by_year))
 
     plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width, align='center')
+    
     plt.title('Movies per year')
     plt.xlabel('Year')
     plt.ylabel('Number of movies')
     plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+
     plt.subplots_adjust(bottom=0.3)
+
     buffer = io.BytesIO()
     plt.savefig(buffer, format='png')
     buffer.seek(0)
@@ -54,7 +54,6 @@ def stadistics_view(request):
 
     image_png = buffer.getvalue()
     buffer.close()
-    graphic = base64.b64decode(image_png)
-    graphic = graphic.decode('utf-8')
+    graphic = base64.b64decode(image_png).decode('latin-1')
 
-    return render(request, 'statics.html', {'graphic': graphic})
+    return render(request, 'statistics.html', {'graphic': graphic})
